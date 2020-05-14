079ad22a7e3ac317fa14868f327243f0f345d019
jenchieh <jcs090218@gmail.com>
2020-05-14 23:38:19 +0800

    Bump version.

--- origami-parsers.el
+++ origami-parsers.el
@@ -2,7 +2,7 @@
 
 ;; Author: Greg Sexton <gregsexton@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; Version: 1.0
+;; Version: 1.1
 ;; Keywords: parsers
 ;; URL: https://github.com/gregsexton/
 
