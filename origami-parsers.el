5dae613da7b08c9e24b1205214fd5e7c3f66cc72
JenChieh <jcs090218@gmail.com>
2021-01-26 15:23:59 +0800

    Bump version

--- origami-parsers.el
+++ origami-parsers.el
@@ -2,7 +2,7 @@
 
 ;; Author: Greg Sexton <gregsexton@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
-;; Version: 2.7
+;; Version: 2.8
 ;; Keywords: parsers
 ;; URL: https://github.com/emacs-origami/origami.el
 
