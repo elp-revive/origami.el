56a38428bace23e141c0cd0a2648d1b074ca9473
Jen-Chieh Shen <jcs090218@gmail.com>
2023-01-01 00:31:14 +0800

    Update copyright year

--- origami-util.el
+++ origami-util.el
@@ -2,7 +2,7 @@
 
 ;; The MIT License (MIT)
 
-;; Copyright (c) 2020-2022 Jen-Chieh Shen
+;; Copyright (c) 2020-2023 Jen-Chieh Shen
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
